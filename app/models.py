6249ab2251da6031044bc4e584e8205c92b281ee
Nabz <bnnabiswa@gmail.com>
2020-05-10 23:22:33 +0300

    create model classes

---
+++ app/models.py
@@ -0,0 +1,90 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin, current_user
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+            self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+    
+    class Comment(db.Model):
+        __tablename__ = 'comments'
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+        post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+        comment = db.Column(db.Text())
+        dateposted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+        def save(self):
+            db.session.add(self)
+            db.session.commit()
+
+        @classmethod
+        def get_comments(cls, post_id):
+            comments = Comment.query.filter_by(post_id=post_id).all()
+            return comments
+
+        def delete(self):
+            db.session.delete(self)
+            db.session.commit()
+
+        def __repr__(self):
+             return f"Comment('{self.content}', '{self.dateposted}')"
+
+class Quotes:
+    def __init__(self,author,id,quote):
+        self.author = author
+        self.id = id
+        self.quote = quote
