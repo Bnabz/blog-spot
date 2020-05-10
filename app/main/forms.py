1698a3fe87bd79deba1991536b001550d041b807
Nabz <bnnabiswa@gmail.com>
2020-05-10 21:48:35 +0300

    create forms classes

---
+++ app/main/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Post')
+
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Post a comment', validators=[Required()])
+    submit = SubmitField('Post')
