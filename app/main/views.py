9c9b3eb603fbc874c2212e7dc5ebabe961d19da1
Nabz <bnnabiswa@gmail.com>
2020-05-11 04:38:27 +0300

    add profile route

--- app/main/views.py
+++ app/main/views.py
@@ -13,3 +13,15 @@ def index():
     quotes = getQuotes()
     posts = Post.query.all()
     return render_template('index.html', quotes=quotes, posts=posts, current_user=current_user)
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    user_id = current_user._get_current_object().id
+    profile_posts = Post.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,profile_posts=profile_posts)
